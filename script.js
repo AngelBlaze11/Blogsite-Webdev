05efc64c283929b07e22bf7df58c7268c2da743e
AngelBlaze11 <angelblaze779@gmail.com>
2020-10-13 01:19:34 +0900

    3 files modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('hello!')
 function Submitform() {
   var one = document.getElementById('name').value
-   var one = document.getElementById('email').value
-   alert ("Thank you, [" + one + "][" + one + "]. We received your message.")
+   var two = document.getElementById('email').value
+   alert ("Thank you, [" + one + "][" + two + "]. We received your message.")
 }
